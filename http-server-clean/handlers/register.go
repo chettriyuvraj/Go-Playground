2339240f33def2ee4683fb410280a3924d7af911
Yuvraj Chettri <chettriyuvraj@gmail.com>
2024-06-05 18:48:32 +0530

    refactor: change package name and setup function name for handlers

--- http-server-clean/handlers/register.go
+++ http-server-clean/handlers/register.go
@@ -1,10 +1,10 @@
-package register
+package handlers
 
 import (
 	"http-server-clean/app"
 	"net/http"
 )
 
-func SetupHandlers(mux *http.ServeMux, config *app.AppConfig) {
+func Register(mux *http.ServeMux, config *app.AppConfig) {
 	mux.Handle("/ok", &app.App{Config: config, Handler: HandleOkRoute})
 }
