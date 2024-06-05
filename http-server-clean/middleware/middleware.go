c1b1cfb1482ab8350c38f2522ccb9cb043ce93e1
Yuvraj Chettri <chettriyuvraj@gmail.com>
2024-06-05 18:36:47 +0530

    refactor: pass appConfig to middleware to access its fields

--- http-server-clean/middleware/middleware.go
+++ http-server-clean/middleware/middleware.go
@@ -1,18 +1,18 @@
 package middleware
 
 import (
-	"log"
+	"http-server-clean/app"
 	"net/http"
 	"time"
 )
 
-func LoggingMiddleware(handler http.Handler) http.HandlerFunc {
+func LoggingMiddleware(handler http.Handler, config *app.AppConfig) http.HandlerFunc {
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, req *http.Request) {
 			timeStart := time.Now()
 			handler.ServeHTTP(w, req)
 			timeTaken := time.Since(timeStart)
-			log.Printf("Path: %s; Method: %s, Time taken: %s", req.URL.EscapedPath(), req.Method, timeTaken)
+			config.Logger.Printf("Path: %s; Method: %s, Time taken: %s", req.URL.EscapedPath(), req.Method, timeTaken)
 		},
 	)
 }
